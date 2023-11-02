a6588ee8a50e1f6d68d011640ba8b531713931f8
Rufat Khaslarov <rufat.khaslarov@orca.security>
2023-11-02 16:21:25 +0300

    Logging

--- dist/reporter.js
+++ dist/reporter.js
@@ -37,8 +37,9 @@ async function run() {
         const slackWebhook = core.getInput('slack-webhook');
         const notifyEmpty = core.getInput('notify-empty') === 'true';
         const excludeLabels = core.getInput('exclude-labels')?.split(',');
+        core.info(`Starting GraphQL request...`);
         const response = await github.queryPRs(token);
-        core.debug('Successful GraphQL response');
+        core.info(`Successful GraphQL response: ${JSON.stringify(response)}`);
         const pullRequests = response?.pullRequests.nodes;
         const repoName = response?.nameWithOwner;
         const readyPRS = pullRequests.filter((pr) => {
@@ -46,6 +47,7 @@ async function run() {
                 pr.labels.nodes.some(label => excludeLabels.includes(label.name));
             return !pr.isDraft && !excluded;
         });
+        core.info(`PRs are ready for review: ${JSON.stringify(readyPRS)}`);
         let text = '';
         if (readyPRS.length === 0) {
             if (notifyEmpty) {
@@ -60,7 +62,7 @@ async function run() {
         }
         const message = (0, message_1.formatSlackMessage)(repoName, text, pullRequests.length, readyPRS.length);
         await axios_1.default.post(slackWebhook, message);
-        core.debug('Successful Slack webhook response');
+        core.info('Successful Slack webhook response');
     }
     catch (error) {
         core.setFailed(error.message);
